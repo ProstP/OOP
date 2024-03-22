e40da26470cc5bbf91aabafc3f8913471de3b1dc
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-03-22 18:07:54 +0300

    Lw2-5-1 Додела программу, сделал тесты

--- lw2/5-1/URLParser/ParseURLTest/ParseURLTest.cpp
+++ lw2/5-1/URLParser/ParseURLTest/ParseURLTest.cpp
@@ -1,6 +1,170 @@
 ﻿#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "../../../../catch.hpp"
+#include "../URLParser/ParseURL.h"
 
-int main()
+SCENARIO("Parse url")
 {
-    std::cout << "Hello World!\n";
+	std::string dummyHost;
+	std::string dummyDocument;
+	int dummyPort = 0;
+	Protocol dummyProtocol = Protocol::HTTP;
+
+	WHEN("Url is invalid")
+	{
+		THEN("False")
+		{
+			CHECK(!ParseURL("fsfsd", dummyProtocol, dummyPort, dummyHost, dummyDocument));
+		}
+	}
+
+	WHEN("Host is empty")
+	{
+		THEN("False")
+		{
+			CHECK(!ParseURL("https://:9000/smt", dummyProtocol, dummyPort, dummyHost, dummyDocument));
+		}
+	}
+
+	WHEN("Url is valid")
+	{
+		THEN("Valid host and empty document")
+		{
+			std::string host;
+			std::string document;
+			ParseURL("https://www.youtube.com", dummyProtocol, dummyPort, host, document);
+			CHECK(host == "www.youtube.com");
+			CHECK(document.empty());
+		}
+	}
+
+	WHEN("Port length more then acceptable value length")
+	{
+		THEN("False")
+		{
+			CHECK(!ParseURL("https://localhost:65536/", dummyProtocol, dummyPort, dummyHost, dummyDocument));
+		}
+	}
+
+	WHEN("Port more then acceptable value")
+	{
+		THEN("False")
+		{
+			CHECK(!ParseURL("https://localhost:65536/", dummyProtocol, dummyPort, dummyHost, dummyDocument));
+		}
+	}
+
+	WHEN("Url with port")
+	{
+		THEN("Valid port")
+		{
+			int port;
+			ParseURL("https://localhost:8000/", dummyProtocol, port, dummyHost, dummyDocument);
+			CHECK(port == 8000);
+		}
+	}
+
+	WHEN("Url with document")
+	{
+		THEN("Valid document")
+		{
+			std::string document;
+			ParseURL("https://vk.com/feed", dummyProtocol, dummyPort, dummyHost, document);
+			CHECK(document == "feed");
+		}
+	}
+
+	WHEN("Url with port and document")
+	{
+		THEN("Valid parts")
+		{
+			int port;
+			std::string host;
+			std::string document;
+			ParseURL("https://localhost:3301/post/1", dummyProtocol, port, host, document);
+			CHECK(port == 3301);
+			CHECK(host == "localhost");
+			CHECK(document == "post/1");
+		}
+	}
+
+	WHEN("Invalid protocol")
+	{
+		THEN("False")
+		{
+			CHECK(!ParseURL("protocol://www.youtube.com", dummyProtocol, dummyPort, dummyHost, dummyDocument));
+		}
+	}
+
+	WHEN("Https protocol")
+	{
+		THEN("Valid protocol and port")
+		{
+			Protocol protocol;
+			int port;
+			ParseURL("https://www.youtube.com", protocol, port, dummyHost, dummyDocument);
+			CHECK(protocol == Protocol::HTTPS);
+			CHECK(port == 443);
+		}
+	}
+
+	WHEN("Http protocol")
+	{
+		THEN("Valid protocol and port")
+		{
+			Protocol protocol;
+			int port;
+			ParseURL("http://www.youtube.com", protocol, port, dummyHost, dummyDocument);
+			CHECK(protocol == Protocol::HTTP);
+			CHECK(port == 80);
+		}
+	}
+
+	WHEN("Ftp protocol")
+	{
+		THEN("Valid protocol and port")
+		{
+			Protocol protocol;
+			int port;
+			ParseURL("ftp://www.youtube.com", protocol, port, dummyHost, dummyDocument);
+			CHECK(protocol == Protocol::FTP);
+			CHECK(port == 21);
+		}
+	}
+
+	WHEN("Https protocol in different register")
+	{
+		THEN("Valid protocol and port")
+		{
+			Protocol protocol;
+			int port;
+			ParseURL("HtTpS://www.youtube.com", protocol, port, dummyHost, dummyDocument);
+			CHECK(protocol == Protocol::HTTPS);
+			CHECK(port == 443);
+		}
+	}
+
+	WHEN("Http protocol in different register")
+	{
+		THEN("Valid protocol and port")
+		{
+			Protocol protocol;
+			int port;
+			ParseURL("HtTP://www.youtube.com", protocol, port, dummyHost, dummyDocument);
+			CHECK(protocol == Protocol::HTTP);
+			CHECK(port == 80);
+		}
+	}
+
+	WHEN("Ftp protocol in different register")
+	{
+		THEN("Valid protocol and port")
+		{
+			Protocol protocol;
+			int port;
+			ParseURL("FtP://www.youtube.com", protocol, port, dummyHost, dummyDocument);
+			CHECK(protocol == Protocol::FTP);
+			CHECK(port == 21);
+		}
+	}
 }
--- lw2/5-1/URLParser/URLParser/ParseURL.cpp
+++ lw2/5-1/URLParser/URLParser/ParseURL.cpp
@@ -2,6 +2,13 @@
 #include <regex>
 #include <map>
 
+const std::map<std::string, Protocol> STRING_TO_PROTOCOL
+{
+	{"http", Protocol::HTTP},
+	{"https", Protocol::HTTPS},
+	{"ftp", Protocol::FTP}
+};
+
 const std::map<Protocol, int> DEFAULT_PORT
 {
 	{Protocol::HTTP, 80},
@@ -9,17 +16,94 @@ const std::map<Protocol, int> DEFAULT_PORT
 	{Protocol::FTP, 21}
 };
 
-bool ParseURL(const std::string& url, Protocol& protocol, int& port, std::string& host, std::string& document)
+bool CheckUrl(const std::string& url)
 {
-	const std::regex pattern("^(http|https|ftp):\/\/\[^\s]+(:[1-65535])*(\/[^\s])*");
+	const std::regex pattern("^([hH][tT][tT][pP][sS]?|[fF][tT][pP])://[\\w-.,]+(:\\d{1,5})?(/[\\w-.,/]+)?/?$");
 
-	std::smatch substr;
-
-	if (std::regex_match(url, pattern))
+	if (!std::regex_match(url, pattern))
 	{
-		host = substr[0].str();
-		return true;
+		return false;
 	}
 
-	return false;
+	return true;
+}
+
+void StringToLower(std::string& str)
+{
+	for (char& c : str)
+	{
+		c = tolower(c);
+	}
+}
+
+void GetProtocol(const std::string& url, Protocol& protocol)
+{
+	const std::regex pattern("^([hH][tT][tT][pP][sS]?|[fF][tT][pP])");
+	std::smatch match;
+
+	std::regex_search(url, match, pattern);
+
+	std::string protocolStr = match[0].str();
+	StringToLower(protocolStr);
+	protocol = STRING_TO_PROTOCOL.at(protocolStr);
+}
+
+bool GetPort(const std::string& url, int& port)
+{
+	const std::regex portPattern(":\\d+");
+	std::smatch portMatch;
+	if (!std::regex_search(url, portMatch, portPattern))
+	{
+		return false;
+	}
+	std::string	portStr = portMatch[0].str();
+	std::string digitsStr = portStr.substr(1, portStr.length() - 1);
+
+	port = std::stoi(digitsStr);
+	return true;
+}
+
+std::string GetHost(const std::string& url)
+{	
+	const std::regex hostPattern("://[\\w-.,]+");
+	std::smatch hostMatch;
+	std::regex_search(url, hostMatch, hostPattern);
+	std::string partWithHost = hostMatch[0].str();
+	return partWithHost.substr(3, partWithHost.length() - 3);
+}
+
+std::string GetDocument(const std::string& url)
+{
+	const std::regex documentPattern("[\\w-.,]/[\\w-.,/]+");
+	std::smatch documentMatch;
+	if (!std::regex_search(url, documentMatch, documentPattern))
+	{
+		return "";
+	}
+	std::string document = documentMatch[0].str();
+	return document.substr(2, document.length() - 2);
+}
+
+bool ParseURL(const std::string& url, Protocol& protocol, int& port, std::string& host, std::string& document)
+{
+	if (!CheckUrl(url))
+	{
+		return false;
+	}
+	
+	GetProtocol(url, protocol);
+	if (!GetPort(url, port))
+	{
+		port = DEFAULT_PORT.at(protocol);
+	}
+	const int maxPortValue = 65535;
+	if (port > maxPortValue)
+	{
+		return false;
+	}
+
+	host = GetHost(url);
+	document = GetDocument(url);
+
+	return true;
 }
